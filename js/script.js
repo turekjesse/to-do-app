abe148988438ed39c8ffaeba7abda0771cc26cc5
turekjesse <turekjesse@gmail.com>
2023-09-23 12:52:36 -0400

    add item func

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 // selectors
-const addBtn = document.getElementById("add-item")
+const itemInput = document.getElementById("item-input")
+const itemList = document.getElementById("item-list")
 
 // functions
 
-// event listeners
-addBtn.addEventListener("click", () => console.log("add item click") )
+const addItem = () => {
+    if (itemInput.value !== "") {
+        let li = document.createElement("li")
+        let span = document.createElement("span")
+        li.innerHTML = itemInput.value
+        itemList.appendChild(li)
+        li.appendChild(span)
+        itemInput.value = ""
+    }
+}
